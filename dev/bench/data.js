c734d606631c65d067bfde30709d5b808a3b8f25
github-action-benchmark <github@users.noreply.github.com>
2026-03-06 20:27:16 +0000

    add Agent VCR Benchmarks (pytest) benchmark result for 879c3730a7e4575ead99da90dd53a3c44b937440

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772828506868,
+  "lastUpdate": 1772828836081,
   "repoUrl": "https://github.com/ixchio/agent-vcr",
   "entries": {
     "Agent VCR Benchmarks": [
@@ -52,6 +52,58 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 5.941377264403885e-7",
             "extra": "mean: 1.1885980001551388 usec\nrounds: 1000"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "priyankapandeykum@gmail.com",
+            "name": "amankumarpandeyin",
+            "username": "ixchio"
+          },
+          "committer": {
+            "email": "priyankapandeykum@gmail.com",
+            "name": "amankumarpandeyin",
+            "username": "ixchio"
+          },
+          "distinct": true,
+          "id": "879c3730a7e4575ead99da90dd53a3c44b937440",
+          "message": "docs: Update GitHub repository",
+          "timestamp": "2026-03-07T01:55:55+05:30",
+          "tree_id": "db7d5c474278f6410b090777cc67bc9e7c06cf38",
+          "url": "https://github.com/ixchio/agent-vcr/commit/879c3730a7e4575ead99da90dd53a3c44b937440"
+        },
+        "date": 1772828835678,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/benchmarks/test_performance.py::TestPerformanceBenchmarks::test_benchmark_recorder_overhead",
+            "value": 56447.482222438826,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000014128198924165423",
+            "extra": "mean: 17.715581999908636 usec\nrounds: 1000"
+          },
+          {
+            "name": "tests/benchmarks/test_performance.py::TestPerformanceBenchmarks::test_benchmark_file_write_speed",
+            "value": 3.2697137499671283,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 305.83716999998956 msec\nrounds: 1"
+          },
+          {
+            "name": "tests/benchmarks/test_performance.py::TestPerformanceBenchmarks::test_benchmark_load_speed",
+            "value": 5.908085485872953,
+            "unit": "iter/sec",
+            "range": "stddev: 0.014235305181730993",
+            "extra": "mean: 169.25956850000526 msec\nrounds: 6"
+          },
+          {
+            "name": "tests/benchmarks/test_performance.py::TestPerformanceBenchmarks::test_benchmark_goto_performance",
+            "value": 733786.0791422804,
+            "unit": "iter/sec",
+            "range": "stddev: 6.379049958669018e-7",
+            "extra": "mean: 1.362795000375172 usec\nrounds: 1000"
           }
         ]
       }
