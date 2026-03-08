9949463c8efe97907792c8b621f8d13935758ec5
amankumarpandeyin <priyankapandeykum@gmail.com>
2026-03-09 01:06:01 +0530

    chore: bump to v0.3.2, fix repository URLs

--- src/agent_vcr/__init__.py
+++ src/agent_vcr/__init__.py
@@ -14,7 +14,7 @@ from agent_vcr.models import (
 from agent_vcr.player import VCRPlayer
 from agent_vcr.recorder import VCRRecorder
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __all__ = [
     "AsyncVCRPlayer",
     "AsyncVCRRecorder",
